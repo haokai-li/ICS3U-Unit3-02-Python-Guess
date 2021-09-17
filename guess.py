0a5ca84d50c84ee514f930c84211e4048783a7a5
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-17 16:12:25 +0000

    guess

---
+++ guess.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This Program guess a number between 0 - 9
+
+
+def main():
+    # This function guess a number between 0 - 9
+
+    # input
+    number = int(input("Enter a number between 0 - 9: "))
+    print("")
+
+    # process
+    if number == 5:
+        # output
+        print("you guessed correct!")
+
+    if number != 5:
+        # output
+        print("you guessed wrong!")
+
+    print("\nDone")
+
+
+if __name__ == "__main__":
+    main()
